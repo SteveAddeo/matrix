c4c9d6c971a648957ead7ee220e13f9052e5b5a6
SteveAddeo <Stephen.Addeo@gmail.com>
2020-07-27 11:49:59 -0700

    Create fkiktools.py

---
+++ fkiktools.py
@@ -0,0 +1,20 @@
+import maya.cmds as mc
+import matrixconstrainttools as mt
+reload(mt)
+
+
+class FKIK(mt.BlendColor):
+    """docstring for FKIK"""
+
+    def __init__(self, arg):
+        super(FKIK, self).__init__()
+        self.arg = arg
+
+    def get_root():
+        pass
+
+    def mk_joint_chains():
+        pass
+
+    def mk_blend():
+        pass
